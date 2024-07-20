e96c6db1321b49bdcd8c5eaa793e71c03dcaab76
Rahul Bhaskar <rahulwebsmart@gmail.com>
2024-07-20 22:49:46 +0530

    News page added

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -125,4 +125,14 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionSay($message = 'Hello')
+    {
+        return $this->render('say', ['message' => $message]);
+    }
 }
